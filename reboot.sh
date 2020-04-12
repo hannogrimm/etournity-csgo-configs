3c1a547e5888c8c7ca468801dbdef485e704d39d
Svvite <dimitrigrimm@yahoo.de>
2020-04-12 19:23:21 +0200

    .

--- reboot.sh
+++ reboot.sh
@@ -1,2 +1,1 @@
-apt-get update && apt-get -y dist-upgrade && git -C ~/presets pull origin master
-&& cp -r ~/presets/wingman/* ~/csgo/csgo && cp ~/presets/start.sh ~/ && cp ~/presets/reboot.sh ~/
+apt-get update && apt-get -y dist-upgrade && git -C ~/presets pull origin master && cp -r ~/presets/wingman/* ~/csgo/csgo && cp ~/presets/start.sh ~/ && cp ~/presets/reboot.sh ~/
