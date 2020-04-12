73652195ad911ff2390fd556810a7a1901111608
Svvite <dimitrigrimm@yahoo.de>
2020-04-12 19:17:50 +0200

    .

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 +mapgroup mg_wingman +map de_train
+~/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 +mapgroup mg_wingman +map de_train
