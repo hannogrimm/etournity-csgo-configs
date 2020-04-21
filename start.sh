f01a7223c477165a6b940d1730adb8fc8b81e279
Svvite <dimitrigrimm@yahoo.de>
2020-04-21 09:12:48 +0200

    Interpreter on start bash

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-steamcmd +login anonymous +force_install_dir /home/ubuntu/csgo +app_update 740 +exit && /home/ubuntu/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 +mapgroup mg_wingman +map de_train
+#!/bin/bash
+steamcmd +login anonymous +force_install_dir /home/ubuntu/csgo +app_update 740 +quit
+/home/ubuntu/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 +mapgroup mg_wingman +map de_train
