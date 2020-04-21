5f1c79d2a0e9e612b19a3e14844a25a28cd22670
Svvite <dimitrigrimm@yahoo.de>
2020-04-21 09:05:07 +0200

    Absolute Paths on start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-steamcmd +login anonymous +force_install_dir ~/csgo +app_update 740 +exit && ~/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 +mapgroup mg_wingman +map de_train
+steamcmd +login anonymous +force_install_dir /home/ubuntu/csgo +app_update 740 +exit && /home/ubuntu/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 +mapgroup mg_wingman +map de_train
