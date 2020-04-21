39ebf76dc6a39af8f5daafba0abff8ed40b9e0de
Svvite <dimitrigrimm@yahoo.de>
2020-04-21 09:33:57 +0200

    Add nohup to start bash

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 steamcmd +login anonymous +force_install_dir /home/ubuntu/csgo +app_update 740 +quit
-/home/ubuntu/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 +mapgroup mg_wingman +map de_train
+nohup /home/ubuntu/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 +mapgroup mg_wingman +map de_train &
