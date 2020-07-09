dfe0dc83d9c62046cff539bb72fe09fbcf949685
Hanno <hannogrimm@gmail.com>
2020-07-09 21:06:50 +0200

    .

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-#nohup /home/ubuntu/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 -autoupdate -steam_dir /home/usr/steamcmd -steamcmd_script /home/ubuntu/update_csgo.txt +mapgroup mg_wingman +map de_train &
+nohup /home/ubuntu/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 -autoupdate -steam_dir /usr/games/steamcmd -steamcmd_script /home/ubuntu/update_csgo.txt +mapgroup mg_wingman +map de_train &
