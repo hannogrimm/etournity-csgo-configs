66b8015064d33852b0a9e1e49ad00f1d4b133da9
Hanno <hannogrimm@gmail.com>
2020-07-09 21:04:01 +0200

    update csgo

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-steamcmd +login anonymous +force_install_dir /home/ubuntu/csgo +app_update 740 +quit
-nohup /home/ubuntu/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 +mapgroup mg_wingman +map de_train &
+#nohup /home/ubuntu/csgo/srcds_run -game csgo -usercon -tickrate 128 -net_port_try 1 -autoupdate -steam_dir /home/usr/steamcmd -steamcmd_script /home/ubuntu/update_csgo.txt +mapgroup mg_wingman +map de_train &
